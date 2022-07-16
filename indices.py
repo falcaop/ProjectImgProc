cc1577e4ef203b07a6c53454f8c1b302244ecc62
Pedro Cisdeli <pedrohcsideli@usp.br>
2022-07-16 17:01:19 -0300

    Indices dev

--- indices.py
+++ indices.py
@@ -18,41 +18,33 @@ More info about sentinel-2 L1C bands at: https://docs.sentinel-hub.com/api/lates
 # Dataset selection, default: outFile
 def dataSetSelect(op):
     if op == 1:
-        return ["data/2018-10-13, Sentinel-2B L1C, B02.tiff",
-                "data/2018-10-13, Sentinel-2B L1C, B03.tiff",
-                "data/2018-10-13, Sentinel-2B L1C, B04.tiff",
-                "data/2018-10-13, Sentinel-2B L1C, B08.tiff"]
+        return ["data/data_01/2018-10-13, Sentinel-2B L1C, B02.tiff",
+                "data/data_01/2018-10-13, Sentinel-2B L1C, B03.tiff",
+                "data/data_01/2018-10-13, Sentinel-2B L1C, B04.tiff",
+                "data/data_01/2018-10-13, Sentinel-2B L1C, B08.tiff"]
     else:
-        return ["data/outFile_B02.tiff",
-                "data/outFile_B03.tiff",
-                "data/outFile_B04.tiff",
-                "data/outFile_B08.tiff"]
+        return ["data/data_02/outFile_B02.tiff",
+                "data/data_02/outFile_B03.tiff",
+                "data/data_02/outFile_B04.tiff",
+                "data/data_02/outFile_B08.tiff"]
 
 # Applies sigmoidal contrast enhancement to all pixels equally
 def contrast(img, k=0.035):
     return (255 / (1 + np.exp(-k * (img.astype(np.int32) - 127)))).astype(np.uint8)
 
-# Applies the contrast adjustment without modifying any chromatic component
-def contrast_hsv(img, k=0.035):
-    img_hsv = mpl.colors.rgb_to_hsv(img)
-    img_hsv[:, :, 2] = contrast(img_hsv[:, :, 2].astype(np.uint32), k=k)
+def getNDVI(img):
+    # Ignoring erros related to NaN pixels
+    np.seterr(divide='ignore', invalid='ignore')
+    red = img[2]
+    NIR = img[3]
+    return (NIR.astype(float)-red.astype(float))/(NIR.astype(float)+red.astype(float))
 
-    return mpl.colors.hsv_to_rgb(img_hsv).astype(np.uint8)
-
-# Normalize an image
-def normalize(img):
-    img = 255 * (img - np.min(img)) / (np.max(img) - np.min(img))
-
-    return img
-
-# Applies highlight
-def highlight(img, rates=np.zeros(img.shape[-1])):
-    img = img.astype(np.float32)
-
-    for index, rate in enumerate(rates):
-        img[:, :, index] = normalize(img[:, :, index] * (1 + rate)) if rate != -1 else np.zeros(img.shape[: 2])
-
-    return img.astype(np.uint8)
+def getNDWI(img):
+    # Ignoring erros related to NaN pixels
+    np.seterr(divide='ignore', invalid='ignore')
+    green = img[1]
+    NIR = img[3]
+    return (green.astype(float)-NIR.astype(float))/(green.astype(float)+NIR.astype(float))
 
 # --- Loading bands blue, green, red and NIR respectively
 bands = dataSetSelect(int(input("Dataset selection [1, 2]: ")))
@@ -64,35 +56,61 @@ for band in bands:
 # No need for a clip (for now) since the raster is relatively small
 # And the there is not much use of computing resorces
 img = np.array(arr, dtype=arr[0].dtype)
-print("Image shape: {}".format(img.shape))
+newImg = contrast(img, k=0.035)
 
-#--- NDVI
-# Ignoring erros related to NaN pixels
-np.seterr(divide='ignore', invalid='ignore')
-
-# Separating bands of interest
-red = img[2]
-NIR = img[3]
-
+#--- Indices
 # Normalized Difference Vegetation Index
-NDVI = (NIR.astype(float) - red.astype(float)) / \
-    (NIR.astype(float) + red.astype(float))
+NDVI = getNDVI(img)
+
+# Altered Normalized Difference Vegetation Index
+_NDVI = getNDVI(newImg)
+
+# Normalized Difference Water Index
+NDWI = getNDWI(img)
+
+# Altered Normalized Difference Water Index
+_NDWI = getNDWI(newImg)
 
 #--- Visualization - Image and histogram
 # Image
-fig, (axImg, axHist) = plt.subplots(1, 2, figsize=(20, 20))
-show(img[[2, 1, 0], :, :], ax=axImg,
+fig, axs = plt.subplots(2, 2, figsize=(15, 7))
+show(img[[2, 1, 0], :, :], ax=axs[0,0],
      title='Image (blue, green, red and NIR bands)')
 
-# Histogram
-rasterio.plot.show_hist(img, ax=axHist, bins=50,
-                        histtype='stepfilled', lw=0.0, stacked=False, alpha=0.8)
-axHist.get_legend().remove()
+show(newImg[[2, 1, 0], :, :], ax=axs[0,1],
+     title='Filtered Image (blue, green, red and NIR bands)')
 
+# Histogram
+rasterio.plot.show_hist(img, ax=axs[1,0], bins=50,
+                        histtype='stepfilled', lw=0.0, stacked=False, alpha=0.8)
+rasterio.plot.show_hist(newImg, ax=axs[1,1], bins=50,
+                        histtype='stepfilled', lw=0.0, stacked=False, alpha=0.8)
+axs[1,0].get_legend().remove()
+axs[1,1].get_legend().remove()
 plt.show()
 
+# --- Indices plot
+fig, axs = plt.subplots(2, 2, figsize=(15, 7))
+
 # NDVI
-plt.title("NDVI")
-plt.imshow(NDVI, cmap="RdYlGn")
-plt.colorbar()
+ndviPlot = axs[0, 0].imshow(NDVI, cmap="RdYlGn")
+axs[0, 0].set_title("NDVI")
+fig.colorbar(ndviPlot, ax=axs[0, 0])
+
+# NDWI
+ndwiPlot = axs[0, 1].imshow(NDWI, cmap="RdYlGn")
+axs[0, 1].set_title("NDWI")
+fig.colorbar(ndwiPlot, ax=axs[0, 1])
+
+# Modified img NDVI
+_ndviPlot = axs[1, 0].imshow(_NDVI, cmap="RdYlGn")
+axs[1, 0].set_title("Altered NDVI")
+fig.colorbar(_ndviPlot, ax=axs[1, 0])
+
+# Modified img NDWI
+_ndwiPlot = axs[1, 1].imshow(_NDWI, cmap="RdYlGn")
+axs[1, 1].set_title("Altered NDWI")
+fig.colorbar(_ndviPlot, ax=axs[1, 1])
+
+
 plt.show()
